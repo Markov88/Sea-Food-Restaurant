5df476be308e917df1386d4dd73c850230f550cc
Markov88 <TodorMarkov88@abv.bg>
2019-10-14 08:06:57 +0300

    sss

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,14 +1,21 @@
-const contacts = document.getElementById('heading_footer_contacts');
-const display = document.querySelector('.display_contacts');
-const displayOut = document.querySelector('.display_heading');
-const formDisplay = document.querySelector('.contacts_form');
 
+const divDisplay = document.createElement('div');
+divDisplay.setAttribute('class', 'display_contacts');
+
+document.body.appendChild(divDisplay);
 contacts.addEventListener('click', () => {
-    display.classList.add('displayy_contacts');
-    displayOut.style.visibility = 'visible';
+     
+    divDisplay.classList.add('displayy_contacts');
+    const formDisplay = document.querySelector('.contacts_form')
     formDisplay.style.display = 'block';
 })
+
 displayOut.addEventListener('click', () => {
-    display.classList.remove('displayy_contacts');
+         display.classList.remove('displayy_contacts');
     formDisplay.style.display = 'none';
 })
+
+
+
+
+........
