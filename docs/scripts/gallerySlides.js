a4673ec0bf841a8452e1bbbe353321aa597b3c30
Markov88 <TodorMarkov88@abv.bg>
2019-10-20 14:49:47 +0300

    sssss

--- docs/scripts/gallerySlides.js
+++ docs/scripts/gallerySlides.js
@@ -1,36 +1,37 @@
 
-
-const divSlides = document.querySelectorAll('.slide');
-const viewGallery = document.querySelector('.viewGallery');
+const divSlides = document.querySelectorAll('.slide')
+const viewGallery = document.querySelector('.viewGallery')
 
 
 const addBtns = function (img, btns) {
-    btns = document.createElement('p');
-    btns.textContent = 'View';
-    img.appendChild(btns).classList.add('imgButton');
-    btns.addEventListener('click', onViewClick);
+    btns = document.createElement('p')
+    btns.textContent = 'View'
+    img.appendChild(btns).classList.add('imgButton')
+    btns.addEventListener('click', onViewClick)
 }
 
 function onViewClick (e) {
-    viewGallery.style.zIndex = '99999';
-    viewGallery.style.opacity = '.97';
+    viewGallery.style.zIndex = '99999'
+    viewGallery.style.opacity = '.97'
 
     e.target.parentElement.children[0].classList.add('currentSlide')
     if (e.target.parentElement.children[0].classList.contains('currentSlide')) {
         console.log('xx')
     }
     // e.target.parentElement.children[0].classList.remove('currentSlide')
+    x(cloneImages)
+    slidersss()
 };
 
 const imagesHover = function (imgs) {
     imgs.addEventListener('mouseenter', function () {
-        this.children[1].style.opacity = '1';
-        this.children[0].style.opacity = '.7';
-    });
+        this.children[1].style.opacity = '1'
+        this.children[0].style.opacity = '.7'
+    })
     imgs.addEventListener('mouseleave', function () {
-        this.children[1].style.opacity = '0';
-        this.children[0].style.opacity = '1';
-    });
+        this.children[1].style.opacity = '0'
+        this.children[0].style.opacity = '1'
+    })
 };
 
 // Array.from(divSlides).forEach(addBtns);
@@ -42,44 +43,46 @@ function x (func) {
 x(addBtns)
 x(imagesHover)
 
-const cloneImages = function (img, createLi) {
-    createLi = document.createElement('li');
+function cloneImages (img, createLi) {
+    createLi = document.createElement('li')
     if (img.children[0].classList.contains('currentSlide')) {
-        console.log('zzzz') // document.querySelector('.track_carousel').appendChild(createLi).classList.add('currentSlide')
+        img.children[0].classList.remove('currentSlide')
+        createLi.classList.add('currentSlide')
     }
     let cloneImg = img.children[0].cloneNode(true)
     createLi.appendChild(cloneImg)
 
     document.querySelector('.track_carousel').appendChild(createLi).classList.add('carousel_slide')
 
-    console.log(img.children[0])
+    console.log(createLi)
 }
-x(cloneImages)
-
-
 
 // ----SLIDERS---
+function slidersss () {
+    if (Array.from(document.querySelector('.track_carousel').children).length > 0) {
+
+        const track = document.querySelector('.track_carousel')
+        const slides = Array.from(track.children)
+        const nextBtn = document.querySelector('.carousel_btn.btn_right')
+        const prevBtn = document.querySelector('.carousel_btn.btn_left')
+        let slideWidth = slides[0].getBoundingClientRect().width
+
+        let setSlidePosition = (slide, index) => {
+            slide.style.left = slideWidth * index + 'px'
+
+        }
+        slides.forEach(setSlidePosition)
 
 
-const track = document.querySelector('.track_carousel');
-const slides = Array.from(track.children);
-const nextBtn = document.querySelector('.carousel_btn.btn_right');
-const prevBtn = document.querySelector('.carousel_btn.btn_left');
-const slideWidth = slides[0].getBoundingClientRect().width;
+        nextBtn.addEventListener('click', e => {
+            const currentSlide = track.querySelector('.currentSlide')
+            const nextSlide = currentSlide.nextElementSibling
+            const amountToMove = nextSlide.style.left
+            track.style.transform = 'translateX(-' + amountToMove + ')'
+            currentSlide.classList.remove('currentSlide')
+            nextSlide.classList.add('currentSlide')
 
-const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + 'px';
+            console.log(currentSlide)
+        })
+    }
 }
-slides.forEach(setSlidePosition);
-
-nextBtn.addEventListener('click', e => {
-    const currentSlide = track.querySelector('.currentSlide');
-    const nextSlide = currentSlide.nextElementSibling;
-    const amountToMove = nextSlide.style.left;
-    track.style.transform = 'translateX(- ' + amountToMove + ')';
-    currentSlide.classList.remove('currentSlide');
-    nextSlide.classList.add('currentSlide');
-
-    console.log(currentSlide)
-})
-
