9454cfa7abb50f762fd46b73949283c8a4666f18
Markov88 <TodorMarkov88@abv.bg>
2019-10-20 17:30:05 +0300

    lasttt

--- docs/scripts/gallerySlides.js
+++ docs/scripts/gallerySlides.js
@@ -2,7 +2,6 @@
 const divSlides = document.querySelectorAll('.slide')
 const viewGallery = document.querySelector('.viewGallery')
 
-
 const addBtns = function (img, btns) {
     btns = document.createElement('p')
     btns.textContent = 'View'
@@ -18,7 +17,8 @@ function onViewClick (e) {
     if (e.target.parentElement.children[0].classList.contains('currentSlide')) {
         console.log('xx')
     }
-    // e.target.parentElement.children[0].classList.remove('currentSlide')
+    // 
+
     x(cloneImages)
     slidersss()
 };
@@ -27,11 +27,11 @@ const imagesHover = function (imgs) {
     imgs.addEventListener('mouseenter', function () {
         this.children[1].style.opacity = '1'
         this.children[0].style.opacity = '.7'
-    })
+    });
     imgs.addEventListener('mouseleave', function () {
         this.children[1].style.opacity = '0'
         this.children[0].style.opacity = '1'
-    })
+    });
 };
 
 // Array.from(divSlides).forEach(addBtns);
@@ -46,21 +46,18 @@ x(imagesHover)
 function cloneImages (img, createLi) {
     createLi = document.createElement('li')
     if (img.children[0].classList.contains('currentSlide')) {
-        img.children[0].classList.remove('currentSlide')
         createLi.classList.add('currentSlide')
+        img.children[0].classList.remove('currentSlide')
     }
+    document.querySelector('.track_carousel').appendChild(createLi).classList.add('carousel_slide')
     let cloneImg = img.children[0].cloneNode(true)
     createLi.appendChild(cloneImg)
-
-    document.querySelector('.track_carousel').appendChild(createLi).classList.add('carousel_slide')
-
     console.log(createLi)
 }
 
 // ----SLIDERS---
 function slidersss () {
     if (Array.from(document.querySelector('.track_carousel').children).length > 0) {
-
         const track = document.querySelector('.track_carousel')
         const slides = Array.from(track.children)
         const nextBtn = document.querySelector('.carousel_btn.btn_right')
@@ -69,27 +66,26 @@ function slidersss () {
 
         let setSlidePosition = (slide, index) => {
             slide.style.left = slideWidth * index + 'px'
-        };
-        slides.forEach(setSlidePosition);
-
+        }
+        slides.forEach(setSlidePosition)
+        console.log(slides)
         const moveToSlide = (track, currentSlide, targetSlide) => {
-            track.style.transform = 'translateX(-' + targetSlide.style.left
-            currentSlide.classLis.remove('currentSlide');
-            targetSlide.classLis.add('currentSlide');
+            track.style.transform = 'translateX(-' + targetSlide.style.left + ')'
+            currentSlide.classLis.remove('.currentSlide')
+            targetSlide.classLis.add('.currentSlide')
+            console.log(targetSlide)
         };
 
         nextBtn.addEventListener('click', e => {
             const currentSlide = track.querySelector('.currentSlide')
             const nextSlide = currentSlide.nextElementSibling
-            moveToSlide(track, current, nextSlide)
-        };
+            moveToSlide(track, currentSlide, nextSlide)
+        })
 
         prevBtn.addEventListener('click', e => {
             const currentSlide = track.querySelector('.currentSlide')
             const prevSlide = currentSlide.previousElementSibling
-            moveToSlide(track, current, prevSlide)
-        });
+            moveToSlide(track, currentSlide, prevSlide)
+        })
     }
-}
-
- 
+};
